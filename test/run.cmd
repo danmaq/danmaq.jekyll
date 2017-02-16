e73da2c98d0139f114187dfd568a821035d2fee5
Shuhei Nomura <info@danmaq.com>
2017-02-16 22:30:52 +0900

    テスト用スクリプト整理。

--- test/run.cmd
+++ test/run.cmd
@@ -7,21 +7,33 @@ echo Docker service and client are required to run this setup.
 exit /b 1
 :PRE_STEP1
 
+<<<<<<< HEAD
 rem ==== CREATE WORKDIR ====
 if not exist tmp md tmp
 if not exist _site md _site
+=======
+if exist tmp rd /S /Q tmp
+md tmp
+if exist _site rd /S /Q _site
+md _site
+>>>>>>> テスト用スクリプト整理。
 
 setlocal
 set PWD=%~dp0
 set PJHOME=%PWD:~0,-6%
 
 rem ==== SET VOL1 ====
+<<<<<<< HEAD
 set DEST=/v/danmaq.jekyll
 set VOL1=%PJHOME%:%DEST%:ro
+=======
+set VOL1=%PJHOME%:/v/t/:ro
+>>>>>>> テスト用スクリプト整理。
 
 rem ==== SET VOL2 ====
 set VARTICLES=%1
 if "%VARTICLES%"=="" set VARTICLES=%PJHOME%\tmp
+<<<<<<< HEAD
 set VOL2=%VARTICLES%:/v/danmaq.articles
 
 rem ==== SET VOL3 ====
@@ -29,9 +41,16 @@ set VOL3=%PJHOME%\_site:/srv/jekyll/_site
 
 rem ==== SET VOL4 ====
 set VOL4=%USERPROFILE%\.ssh\id_rsa:/root/.ssh/id_rsa:ro
+=======
+set VOL2=%VARTICLES%:/v/a/
+>>>>>>> テスト用スクリプト整理。
+
+rem ==== SET VOL3 ====
+set VOL3=%PJHOME%\_site:/srv/jekyll/_site
 
 set CONTAINER=jekyll
 set IMAGE=danmaq/jekyll-git-redcarpet:github-pages
+<<<<<<< HEAD
 set GIT_NAME="jekyll bot by Shuhei Nomura"
 set GIT_EMAIL="info@danmaq.com"
 set LOOP="while true; do sleep 1; done"
@@ -40,6 +59,10 @@ docker run -d -p 4000:4000 ^
     -v %VOL1% -v %VOL2% -v %VOL3% -v %VOL4% ^
     -e GIT_NAME=%GIT_NAME% -e GIT_EMAIL=%GIT_EMAIL% ^
     --name %CONTAINER% %IMAGE% sh -c %LOOP%
+=======
+set LOOP="mkdir /root/.ssh; while true; do sleep 1; done"
+docker run -d -p 4000:4000 -v %VOL1% -v %VOL2% --name %CONTAINER% %IMAGE% sh -c %LOOP%
+>>>>>>> テスト用スクリプト整理。
 echo "Please open via browser: http://localhost:4000"
 
 docker exec -it %CONTAINER% %DEST%/test/incontainer
