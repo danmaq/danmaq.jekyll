c8bb24a7f41eca7d16444a5fd62b663ebb972c33
Shuhei Nomura <info@danmaq.com>
2017-02-16 22:30:52 +0900

    テスト用スクリプト整理。

--- test/run.cmd
+++ test/run.cmd
@@ -6,19 +6,30 @@ echo Docker service and client are required to run this setup.
 exit /b 1
 :PRE_STEP1
 
-setlocal
-set VHOME=%~dp0
-set VOL1=%VHOME:~0,-6%:/v/t/:ro
-
 if exist tmp rd /S /Q tmp
 md tmp
+if exist _site rd /S /Q _site
+md _site
+
+setlocal
+set PWD=%~dp0
+set PJHOME=%PWD:~0,-6%
+
+rem ==== SET VOL1 ====
+set VOL1=%PJHOME%:/v/t/:ro
+
+rem ==== SET VOL2 ====
 set VARTICLES=%1
-if "%VARTICLES%"=="" set VARTICLES=%VHOME:~0,-6%\tmp
+if "%VARTICLES%"=="" set VARTICLES=%PJHOME%\tmp
 set VOL2=%VARTICLES%:/v/a/
+
+rem ==== SET VOL3 ====
+set VOL3=%PJHOME%\_site:/srv/jekyll/_site
 
 set CONTAINER=jekyll
 set IMAGE=danmaq/jekyll-git-redcarpet:github-pages
-docker run -d -p 4000:4000 -v %VOL1% -v %VOL2% --name %CONTAINER% %IMAGE% sh -c "mkdir /root/.ssh;while true; do sleep 1; done"
+set LOOP="mkdir /root/.ssh; while true; do sleep 1; done"
+docker run -d -p 4000:4000 -v %VOL1% -v %VOL2% --name %CONTAINER% %IMAGE% sh -c %LOOP%
 echo "Please open via browser: http://localhost:4000"
 
 docker exec -it %CONTAINER% /v/test/incontainer
