c25aafcb8a210e61abf4b395851c608e84138882
Shuhei Nomura <info@danmaq.com>
2017-02-16 13:03:43 +0900

    記事用の追加ボリュームをマウントできるようにした。とりまWindows用だけ。

--- test/run.cmd
+++ test/run.cmd
@@ -7,10 +7,18 @@ exit /b 1
 :PRE_STEP1
 
 setlocal
-set VSRC=%~dp0
+set VHOME=%~dp0
+set VOL1=%VHOME:~0,-6%:/v/t/:ro
+
+if exist tmp rd /S /Q tmp
+md tmp
+set VARTICLES=%1
+if "%VARTICLES%"=="" set VARTICLES=%VHOME:~0,-6%\tmp
+set VOL2=%VARTICLES%:/v/a/
+
 set CONTAINER=jekyll
 set IMAGE=danmaq/jekyll-git-redcarpet:github-pages
-docker run -d -p 4000:4000 -v %VSRC:~0,-1%:/v/:ro --name %CONTAINER% %IMAGE% sh -c "mkdir /root/.ssh;while true; do sleep 1; done"
+docker run -d -p 4000:4000 -v %VOL1% -v %VOL2% --name %CONTAINER% %IMAGE% sh -c "mkdir /root/.ssh;while true; do sleep 1; done"
 echo "Please open via browser: http://localhost:4000"
 
 docker exec -it %CONTAINER% /v/test/incontainer
